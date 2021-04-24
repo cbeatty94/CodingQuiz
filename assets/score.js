81934e268508a02e3b7a9dc7a40bd91fbbe28035
Burke <beatty.burke@gmail.com>
2021-04-24 14:20:34 -0400

    added function into score.js and created assets folder

---
+++ assets/score.js
@@ -0,0 +1,13 @@
+var endScores = JSON.parse(localStorage.getItem('scores'));
+var resetHStn = document.getElementById('clear');
+var tryAgainBtn = document.getElementById('tryAgain');
+
+if (endScores === null){
+}else{
+    for (let index = 0; index < endScores.length; index++) {
+       var a = document.createElement('li');
+       var b = document.createTextNode(`${index + 1}, ${endScores[index].playerInitials.toUpperCase()} - ${endScores[index].userScore}`);
+       a.appendChild(b);
+       document.querySelector('#highScore').appendChild(a);
+       a.className += 'userList';
+    }}
